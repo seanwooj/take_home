e176929f5e5ab27164c4051609632cb53d78a0cc
seanwooj <seanwoojinkim@gmail.com>
2013-03-23 09:16:57 -0700

    initializing git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EduApp::Application.config.secret_token = '3a4fef18dcbee725cbf3adce8477b9d9fc5cf908884ea457898f5e1a08bb5ceb481e00540847ef92033d2399800ca4426062d37b2498d65c645912f014fe8b63'
